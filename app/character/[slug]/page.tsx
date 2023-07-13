88bbf7b7a5f55d5f3dcfb186eca0fd78302cd81b
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2023-07-13 12:20:36 -0300

    Character Page Portal Effect

--- app/character/[slug]/page.tsx
+++ app/character/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useAppSelector, useAppDispatch } from "@/hooks/useReduxStore";
 import { fetchCharacter } from "@/store/features/characters/characterSlice";
 
@@ -49,61 +49,80 @@ const CharacterPage = ({ params }: Props) => {
   }, [dispatch, params]);
 
   return (
-    <motion.div
-      className="max-w-7xl mx-auto overflow-x-hidden px-5 md:px-0"
-      variants={characterPageVariants}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={transition}
-    >
+    <div className="max-w-7xl mx-auto overflow-x-hidden px-5 md:px-0">
       {/* {loading && <p>Loading...</p>}
       {!loading && !character && <p>Character not found</p>}
       {error && <p>{error}</p>} */}
-      {!loading && character && (
-        <div className="h-screen md:grid md:grid-cols-5 md:gap-5 justify-center items-center">
-          <div className="col-span-2 flex flex-col justify-center items-center gap-5">
+      <AnimatePresence mode="wait">
+        {loading && (
+          <motion.div
+            key={1}
+            className="h-screen flex justify-center items-center"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100}}
+          >
             <Image
-              src={character.image}
-              alt={character.name}
+              src="/giphy.gif"
+              alt="loading"
               width="500"
               height="500"
-              className="rounded-full w-[300px] h-[300px] md:w-[500px] md:h-[500px]"
             />
-            <h3 className="text-2xl font-bold text-gray-800">
-              {character.name}
-            </h3>
-          </div>
+          </motion.div>
+        )}
+        {!loading && character && (
+          <motion.div 
+            key={2}
+            className="h-screen md:grid md:grid-cols-5 md:gap-5 justify-center items-center"
+            variants={characterPageVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={transition}  
+          >
+            <div className="col-span-2 flex flex-col justify-center items-center gap-5">
+              <Image
+                src={character.image}
+                alt={character.name}
+                width="500"
+                height="500"
+                className="rounded-full w-[300px] h-[300px] md:w-[500px] md:h-[500px]"
+              />
+              <h3 className="text-2xl font-bold text-gray-800">
+                {character.name}
+              </h3>
+            </div>
 
-          <div className="col-span-3 flex flex-col">
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Origin</h3>
-              <p className="p-5">{character.origin.name}</p>
+            <div className="col-span-3 flex flex-col">
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Origin</h3>
+                <p className="p-5">{character.origin.name}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Location</h3>
+                <p className="p-5">{character.location.name}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Status</h3>
+                <p className="p-5">{character.status}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Species</h3>
+                <p className="p-5">{character.species}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Gender</h3>
+                <p className="p-5">{character.gender}</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-gray-800">Type</h3>
+                <p className="p-5">{character.type ? character.type : "-"}</p>
+              </div>
             </div>
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Location</h3>
-              <p className="p-5">{character.location.name}</p>
-            </div>
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Status</h3>
-              <p className="p-5">{character.status}</p>
-            </div>
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Species</h3>
-              <p className="p-5">{character.species}</p>
-            </div>
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Gender</h3>
-              <p className="p-5">{character.gender}</p>
-            </div>
-            <div>
-              <h3 className="text-2xl font-bold text-gray-800">Type</h3>
-              <p className="p-5">{character.type ? character.type : "-"}</p>
-            </div>
-          </div>
-        </div>
-      )}
-    </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
